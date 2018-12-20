83b4633accfff05f596dd18290b859c69e2edead
cwooz <woosley.christopher@gmail.com>
2018-12-20 11:53:14 -0500

    'update'

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 function clickedButton() {
   document.querySelector('.container').addEventListener('click', function() {
-    console.log(this);
+    // console.log(this);
   });  
 };
 
 clickedButton();
 
 // console.log('app is connected?');
+
+const buttons = document.querySelectorAll('.container');
+
+const buttonsArr = Array.from(buttons);
+buttonsArr.forEach(cur => {
+  cur.addEventListener('click', () => {
+    console.log('You clicked the button!');
+    console.log(cur);
+  });
+});
+
+Array.from(buttons).forEach(cur => cur.addEventListener('click', () => {
+  console.log('You clicked the button!!')
+}));
