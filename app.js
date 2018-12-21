ace279a139adc2c0eda46847366952c6d0f6cb16
cwooz <woosley.christopher@gmail.com>
2018-12-20 21:48:09 -0500

    'update'

--- app.js
+++ app.js
@@ -10,14 +10,63 @@ clickedButton();
 
 const buttons = document.querySelectorAll('.container');
 
-const buttonsArr = Array.from(buttons);
-buttonsArr.forEach(cur => {
-  cur.addEventListener('click', () => {
-    console.log('You clicked the button!');
-    console.log(cur);
-  });
-});
+// const buttonsArr = Array.from(buttons);
+// buttonsArr.forEach(cur => {
+//   cur.addEventListener('click', () => {
+//     console.log('You clicked the button!');
+//     console.log(cur);
+//   });
+// });
 
 Array.from(buttons).forEach(cur => cur.addEventListener('click', () => {
-  console.log('You clicked the button!!')
+  // console.log('You clicked the button!!')
+
+  // ('.item-header').innerHTML
 }));
+
+
+
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    // console.log('you clicked a button!!!')
+
+    const key = e.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+
+    if (!action) {
+      console.log('number key!');
+      if (displayedNum === '0') {
+        display.textContent = keyContent;
+      }
+    }
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide' 
+    ) {
+      console.log('operator key!');
+    }
+
+    if (action === 'decimal') {
+      console.log('decimal key');
+    }
+
+    if (action === 'clear') {
+      console.log('clear key!');
+    }
+
+    if (action === 'calculate') {
+      console.log('calculate key!');
+    }
+
+
+  }
+});
